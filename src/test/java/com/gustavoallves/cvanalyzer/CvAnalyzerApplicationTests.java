0111ffa9374e6df5e10c3f1c9728721f334fbd66
gustavo <gustavoallvesdev@gmail.com>
2026-01-10 23:12:21 -0300

    Add initial project structure with configuration files and dependencies

---
+++ src/test/java/com/gustavoallves/cvanalyzer/CvAnalyzerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.gustavoallves.cvanalyzer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CvAnalyzerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
